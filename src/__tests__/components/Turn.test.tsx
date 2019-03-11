5e0d75d9bb5b5ed2b2b4111ce97098f2c71c806c
HSZemi <hszemi@users.noreply.github.com>
2019-03-11 21:19:28 +0100

    Restore Turn snapshot test

--- src/__tests__/components/Turn.test.tsx
+++ src/__tests__/components/Turn.test.tsx
@@ -3,7 +3,18 @@ import * as React from 'react';
 import Turn from "../../components/Turn";
 import {default as ModelTurn} from "../../models/Turn";
 
-it('gets rendered', () => {
+it('labels the turn with PICK', () => {
     const turn = enzyme.shallow(<Turn turn={ModelTurn.HOST_PICK} turnNumber={0}/>);
+    expect(turn.find(".turn-host").text()).toEqual('PICK');
+});
+
+it('labels the turn with xPICK', () => {
+    const turn = enzyme.shallow(<Turn turn={ModelTurn.HOST_HIDDEN_EXCLUSIVE_PICK} turnNumber={0}/>);
+    expect(turn.find(".turn-host").text()).toEqual('xPICK');
+});
+
+it('labels the turn with GPICK', () => {
+    const turn = enzyme.shallow(<Turn turn={ModelTurn.HOST_GLOBAL_PICK} turnNumber={0}/>);
+    expect(turn.find(".turn-host").text()).toEqual('gPICK');
     expect(turn).toMatchSnapshot();
 });
