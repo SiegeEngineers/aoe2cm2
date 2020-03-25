c218381ed09c5c36a8f070bd98c29c1c1d1b2785
HSZemi <hszemi@users.noreply.github.com>
2020-03-26 00:28:01 +0100

    Change attribution in the footer
    as asked for by pip

--- src/components/menu/Footer.tsx
+++ src/components/menu/Footer.tsx
@@ -5,8 +5,9 @@ class Footer extends React.Component<object, object> {
         return (
             <div className="footer">
                 <div style={{textAlign: 'center'}} className="text-primary">
-                    Created by pip (2015-2017), <a href="https://aoe2.se">Siege Engineers&nbsp;
-                    <img className='inline-logo' src="/images/SiegeEngineers.png"/></a> (2019).<br/>
+                    Created by <a href="https://aoe2.se">Siege Engineers&nbsp;
+                    <img className='inline-logo' src="/images/SiegeEngineers.png"/></a> (2019-2020),
+                    based on the first version by pip.<br/>
                     <b>WARNING: THIS VERSION OF aoe2cm IS STILL UNDER HEAVY DEVELOPMENT.
                         DO NOT EXPECT IT TO WORK.
                         DO NOT USE IT FOR ANYTHING SERIOUS.</b>
