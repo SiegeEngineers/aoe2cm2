dd5c4356d65512c763af94959d020c01f0ee78ce
HSZemi <hszemi@users.noreply.github.com>
2020-03-25 23:59:13 +0100

    Prettify role selection buttons

--- src/components/draft/RoleModal.tsx
+++ src/components/draft/RoleModal.tsx
@@ -30,15 +30,45 @@ class RoleModal extends React.Component<IProps, object> {
                         <p>
                             <button className='pure-button' onClick={() => {
                                 this.props.setRoleCallback(Player.NONE)
-                            }}><Trans>rolemodal.role.spectator</Trans>
+                            }}>
+                                <div className="role pure-u-1-12">
+                                    <div className="box-content visible">
+                                        <div className="stretchy-wrapper">
+                                            <div className="stretchy-image">
+                                                <img src="/images/spectate.png" alt="Spectate"/>
+                                            </div>
+                                            <div className="stretchy-text"><Trans>rolemodal.role.spectator</Trans></div>
+                                        </div>
+                                    </div>
+                                </div>
                             </button>
                             <button className='pure-button' onClick={() => {
                                 this.props.setRoleCallback(Player.HOST)
-                            }} disabled={this.props.hostConnected}><Trans>rolemodal.role.host</Trans>
+                            }} disabled={this.props.hostConnected}>
+                                <div className="role pure-u-1-12">
+                                    <div className="box-content visible">
+                                        <div className="stretchy-wrapper">
+                                            <div className="stretchy-image">
+                                                <img src="/images/host.png" alt="Spectate"/>
+                                            </div>
+                                            <div className="stretchy-text"><Trans>rolemodal.role.host</Trans></div>
+                                        </div>
+                                    </div>
+                                </div>
                             </button>
                             <button className='pure-button' onClick={() => {
                                 this.props.setRoleCallback(Player.GUEST)
-                            }} disabled={this.props.guestConnected}><Trans>rolemodal.role.guest</Trans>
+                            }} disabled={this.props.guestConnected}>
+                                <div className="role pure-u-1-12">
+                                    <div className="box-content visible">
+                                        <div className="stretchy-wrapper">
+                                            <div className="stretchy-image">
+                                                <img src="/images/guest.png" alt="Spectate"/>
+                                            </div>
+                                            <div className="stretchy-text"><Trans>rolemodal.role.guest</Trans></div>
+                                        </div>
+                                    </div>
+                                </div>
                             </button>
                         </p>
                     </div>
