d6bd2057653a810daedabfa3a614775ffa3dab92
HSZemi <hszemi@users.noreply.github.com>
2020-03-28 02:15:23 +0100

    Remove development warning from footer

--- src/components/menu/Footer.tsx
+++ src/components/menu/Footer.tsx
@@ -6,11 +6,11 @@ class Footer extends React.Component<object, object> {
             <div className="footer">
                 <div style={{textAlign: 'center'}} className="text-primary">
                     Created by <a href="https://aoe2.se">Siege Engineers&nbsp;
-                    <img className='inline-logo' src="/images/SiegeEngineers.png"/></a> (2019-2020),
-                    based on the first version by pip.<br/>
-                    <b>WARNING: THIS VERSION OF aoe2cm IS STILL UNDER HEAVY DEVELOPMENT.
-                        DO NOT EXPECT IT TO WORK.
-                        DO NOT USE IT FOR ANYTHING SERIOUS.</b>
+                    <img alt='SiegeEngineers logo' className='inline-logo'
+                         src="/images/SiegeEngineers.png"/></a> (2019-2020),
+                    based on the first version by pip
+                    &#8196;·&#8196;
+                    <a href="https://github.com/SiegeEngineers/aoe2cm2">GitHub</a>
                 </div>
             </div>
         );
