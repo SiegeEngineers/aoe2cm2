869498c231ac8c486cdbe1757382cef33111d458
Lalit Patel <lalitpatel@users.noreply.github.com>
2020-06-21 22:43:06 +0530

    Extract <TurnTag> as a new component
    Extract `<TurnTag>` as a new component so that it can be reused in `<TurnExplanation>`

--- src/components/PresetEditor/TurnExplanation.tsx
+++ src/components/PresetEditor/TurnExplanation.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 import {withTranslation} from "react-i18next";
+import {default as ModelTurn} from "../../models/Turn";
+import Exclusivity from "../../constants/Exclusivity";
+import TurnTag from "../draft/TurnTag";
+import Player from "../../constants/Player";
+import Action from "../../constants/Action";
 
 const TurnExplanation = () =>
     <React.Fragment>
@@ -25,49 +30,56 @@ const TurnExplanation = () =>
         <p>A <span className="tag has-text-weight-bold is-light is-success">PICK</span> adds a usable civilisation for a
             player.</p>
         <ul>
-            <li>A <span className="tag">GLOBAL PICK</span> or <div className="tags has-addons"><span
-                className="tag has-text-weight-bold is-success">G</span><span
-                className="tag has-text-weight-bold is-success is-light">PICK</span></div> prevents
-                either player from picking the civilisation again in
-                a future turn.
+            <li>A <span className="tag">GLOBAL PICK</span> or <TurnTag turn={new ModelTurn(
+                Player.HOST,
+                Action.PICK,
+                Exclusivity.GLOBAL
+            )}/> prevents either player from picking the civilisation again in a future turn.
             </li>
-            <li>An <span className="tag">EXCLUSIVE PICK</span> or <div className="tags has-addons"><span
-                className="tag has-text-weight-bold is-success">E</span><span
-                className="tag has-text-weight-bold is-success is-light">PICK</span></div>prevents
-                only the picking player from picking the
-                civilisation again in a future turn.
+            <li>An <span className="tag">EXCLUSIVE PICK</span> or <TurnTag turn={new ModelTurn(
+                Player.HOST,
+                Action.PICK,
+                Exclusivity.EXCLUSIVE
+            )}/> prevents only the picking player from picking the civilisation again in a future turn.
             </li>
-            <li>A <span className="tag">NONEXCLUSIVE PICK</span> or <div className="tags has-addons"><span
-                className="tag has-text-weight-bold is-success">N</span><span
-                className="tag has-text-weight-bold is-success is-light">PICK</span></div>does
-                neither of those things, meaning players are still
+            <li>A <span className="tag">NONEXCLUSIVE PICK</span> or <TurnTag turn={new ModelTurn(
+                Player.HOST,
+                Action.PICK,
+                Exclusivity.NONEXCLUSIVE
+            )}/> does neither of those things, meaning players are still
                 able to pick the civilisation again in a future turn.
             </li>
         </ul>
         <p>A <span className="tag has-text-weight-bold is-light is-danger">BAN</span> marks civilisation as not
             available for future turns.</p>
         <ul>
-            <li>A <span className="tag">GLOBAL BAN</span> or <div className="tags has-addons"><span
-                className="tag has-text-weight-bold is-danger">G</span><span
-                className="tag has-text-weight-bold is-danger is-light">BAN</span></div>&nbsp;
-                prevents either player from picking the civilisation in a future turn.
+            <li>A <span className="tag">GLOBAL BAN</span> or <TurnTag turn={new ModelTurn(
+                Player.HOST,
+                Action.BAN,
+                Exclusivity.GLOBAL
+            )}/> prevents either player from picking the civilisation in a future turn.
             </li>
-            <li>An <span className="tag">EXCLUSIVE BAN</span> or <div className="tags has-addons"><span
-                className="tag has-text-weight-bold is-danger">E</span><span
-                className="tag has-text-weight-bold is-danger is-light">BAN</span></div>&nbsp;
-                prevents only the opponent from picking the civilisation in a future turn and the player from banning
+            <li>An <span className="tag">EXCLUSIVE BAN</span> or <TurnTag turn={new ModelTurn(
+                Player.HOST,
+                Action.BAN,
+                Exclusivity.EXCLUSIVE
+            )}/> prevents only the opponent from picking the civilisation in a future turn and the player from banning
                 the civilisation again in a future turn.
             </li>
-            <li>A <span className="tag">NONEXCLUSIVE BAN</span> or <div className="tags has-addons"><span
-                className="tag has-text-weight-bold is-danger">N</span><span
-                className="tag has-text-weight-bold is-danger is-light">BAN</span></div>&nbsp;
-                prevents only the opponent from picking the civilisation in a future turn, and the player may ban the
+            <li>A <span className="tag">NONEXCLUSIVE BAN</span> or <TurnTag turn={new ModelTurn(
+                Player.HOST,
+                Action.BAN,
+                Exclusivity.NONEXCLUSIVE
+            )}/> prevents only the opponent from picking the civilisation in a future turn, and the player may ban the
                 the civilisation again in a future turn, which makes no sense, so you probably want to use <span
                     className="tag">EXCLUSIVE</span> instead anyway.
             </li>
         </ul>
-        <p>A <span className="tag has-text-weight-bold is-light is-link">SNIPE</span> allows a player to remove a
-            civilisation again which the opponent has
+        <p>A <TurnTag turn={new ModelTurn(
+            Player.HOST,
+            Action.SNIPE,
+            Exclusivity.GLOBAL
+        )}/> allows a player to remove a civilisation again which the opponent has
             picked in a previous turn. The GLOBAL, EXCLUSIVE and NONEXCLUSIVE modifiers have no meaning here.</p>
 
         <h4>Modifiers</h4>
@@ -79,9 +91,13 @@ const TurnExplanation = () =>
 
         <h4>Admin Turns</h4>
 
-        <p>The <span className="tag">REVEAL_PICKS</span>, <span className="tag">REVEAL_BANS</span> and <span
-            className="tag">REVEAL_SNIPES</span> turns reveal the
-            respective hidden actions that were executed before.<br/>
+        <p>Admin turns or  <TurnTag turn={new ModelTurn(
+            Player.NONE,
+            Action.REVEAL_ALL,
+            Exclusivity.GLOBAL
+        )}/> reveals the previous set of actions which were hidden. The <span className="tag">REVEAL_PICKS</span>,
+            <span className="tag">REVEAL_BANS</span> and <span className="tag">REVEAL_SNIPES</span> turns reveal the
+            respective hidden actions that were executed before.
             <span className="tag">REVEAL_ALL</span> reveals all hidden actions that were executed before.
         </p>
     </React.Fragment>
--- src/components/draft/Turn.tsx
+++ src/components/draft/Turn.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import {default as ModelTurn} from '../../models/Turn'
-import {Trans, WithTranslation, withTranslation} from "react-i18next";
+import {WithTranslation, withTranslation} from "react-i18next";
 import Player from "../../constants/Player";
-import Exclusivity from "../../constants/Exclusivity";
-import Action from "../../constants/Action";
+import TurnTag from "./TurnTag";
 
 interface IProps extends WithTranslation {
     turn: ModelTurn;
@@ -40,17 +39,6 @@ class Turn extends React.Component<IProps, IState> {
     public render() {
         const turn: ModelTurn = this.props.turn;
 
-        let prefix: string = '';
-        if (turn.player !== Player.NONE && turn.action !== Action.SNIPE) {
-            if (turn.exclusivity === Exclusivity.GLOBAL) {
-                prefix = 'g';
-            } else if (turn.exclusivity === Exclusivity.NONEXCLUSIVE) {
-                prefix = 'n';
-            } else if (turn.exclusivity === Exclusivity.EXCLUSIVE) {
-                prefix = 'e';
-            }
-        }
-
         const player = turn.player.toString().toLowerCase();
         let action: string = turn.action.toString().toLowerCase();
         if (action.includes('reveal')) {
@@ -68,39 +56,10 @@ class Turn extends React.Component<IProps, IState> {
             turnClassName += ' active';
         }
 
-        let tagClassName = 'tag';
-        switch (turn.action) {
-            case Action.PICK:
-                tagClassName += ' is-success';
-                break;
-            case Action.BAN:
-                tagClassName += ' is-danger';
-                break;
-            case Action.SNIPE:
-                tagClassName += ' is-link';
-                break;
-            case Action.REVEAL_ALL:
-            case Action.REVEAL_BANS:
-            case Action.REVEAL_PICKS:
-            case Action.REVEAL_SNIPES:
-                tagClassName += ' has-background-grey-lighter';
-                break;
-        }
-        let tagPrefixClassName = tagClassName + ' is-dark';
-        let prefixTag: React.ReactElement = <span/>;
-        tagClassName += ' is-light'
-        let tagGroupClassName = 'tags';
-        if (prefix) {
-            prefixTag = <span className={tagPrefixClassName}>{prefix}</span>
-            tagGroupClassName += ' has-addons'
-        }
-
         return (
             <div title={toTitle(turn)} className={turnClassName}>
                 <div className='bar'/>
-                <div className={tagGroupClassName}>
-                    <Trans>{prefixTag}<span className={tagClassName}>{action}</span></Trans>
-                </div>
+                <TurnTag turn={this.props.turn}/>
             </div>
         );
     }
