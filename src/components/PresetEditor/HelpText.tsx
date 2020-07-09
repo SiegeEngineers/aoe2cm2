f09ba91f42f9eef6ecc712689896e32ca4f46f25
Lalit Patel <lalitpatel@users.noreply.github.com>
2020-07-09 17:48:47 +0530

    Add translations

--- src/components/PresetEditor/HelpText.tsx
+++ src/components/PresetEditor/HelpText.tsx
@@ -6,143 +6,181 @@ import Player from "../../constants/Player";
 import Action from "../../constants/Action";
 import Exclusivity from "../../constants/Exclusivity";
 
-const Basics = () =>
+export const HelpBasics = withTranslation()(() =>
     <>
-        <h4>Basics</h4>
+        <h4><Trans i18nKey='instructions.basics.title'>Basics</Trans></h4>
         <p>
-            <Trans i18nKey='instructions.1'>Captains mode is a turn-based civilization picker.
+            <Trans i18nKey='instructions.basics.1'>Captains mode is a turn-based civilization picker.
                 Each captain can pick and ban civilizations in a predefined order. Bans
                 prevent the opponent's captain from picking the civilizations.</Trans>
         </p>
         <p>
-            <Trans i18nKey='instructions.2'>For each turn captains have <b>30 seconds</b>. In
+            <Trans i18nKey='instructions.basics.2'>For each turn captains have <b>30 seconds</b>. In
                 case of timeout a random civilization is picked for the captain or no civilization
                 gets banned.</Trans>
         </p>
-    </>;
+    </>
+);
 
-const HostAndGuestTurns = () =>
+export const HelpHostAndGuestTurns = withTranslation()(() =>
     <>
-        <h4>Host and Guest turns</h4>
+        <h4><Trans i18nKey='instructions.hostAndGuestTurns.title'>Host and Guest turns</Trans></h4>
 
-        <p>Turns have a main action (<span className="tag has-text-weight-bold is-light is-success">PICK</span>, <span
-            className="tag has-text-weight-bold is-light is-danger">BAN</span>, <span
-            className="tag has-text-weight-bold is-light is-link">SNIPE</span>) and a few possible modifiers.</p>
-        <p>A <span className="tag has-text-weight-bold is-light is-success">PICK</span> adds a usable civilisation for a
-            player.</p>
+        <p><Trans i18nKey='instructions.hostAndGuestTurns.1'>Turns have a main action (
+            <span className="tag has-text-weight-bold is-light is-success">PICK</span>,
+            <span className="tag has-text-weight-bold is-light is-danger">BAN</span>,
+            <span className="tag has-text-weight-bold is-light is-link">SNIPE</span>)
+            and a few possible modifiers.</Trans></p>
+
+        <p><Trans i18nKey='instructions.hostAndGuestTurns.2'>A
+            <span className="tag has-text-weight-bold is-light is-success">PICK</span>
+            adds a usable civilisation for a player.</Trans></p>
         <ul>
-            <li>A <span className="tag">GLOBAL PICK</span> or <TurnTag turn={new ModelTurn(
+            <li><Trans i18nKey='instructions.hostAndGuestTurns.3'>
+                A <span className="tag">GLOBAL PICK</span> or <TurnTag turn={new ModelTurn(
                 Player.HOST,
                 Action.PICK,
                 Exclusivity.GLOBAL
             )}/> prevents either player from picking the civilisation again in a future turn.
+            </Trans>
             </li>
-            <li>An <span className="tag">EXCLUSIVE PICK</span> or <TurnTag turn={new ModelTurn(
+            <li><Trans i18nKey='instructions.hostAndGuestTurns.4'>
+                An <span className="tag">EXCLUSIVE PICK</span> or <TurnTag turn={new ModelTurn(
                 Player.HOST,
                 Action.PICK,
                 Exclusivity.EXCLUSIVE
             )}/> prevents only the picking player from picking the civilisation again in a future turn.
+            </Trans>
             </li>
-            <li>A <span className="tag">NONEXCLUSIVE PICK</span> or <TurnTag turn={new ModelTurn(
+            <li><Trans i18nKey='instructions.hostAndGuestTurns.5'>
+                A <span className="tag">NONEXCLUSIVE PICK</span> or <TurnTag turn={new ModelTurn(
                 Player.HOST,
                 Action.PICK,
                 Exclusivity.NONEXCLUSIVE
             )}/> does neither of those things, meaning players are still
                 able to pick the civilisation again in a future turn.
+            </Trans>
             </li>
         </ul>
-        <p>A <span className="tag has-text-weight-bold is-light is-danger">BAN</span> marks civilisation as not
-            available for future turns.</p>
+        <p><Trans i18nKey='instructions.hostAndGuestTurns.6'>
+            A <span className="tag has-text-weight-bold is-light is-danger">BAN</span> marks civilisation as not
+            available for future turns.
+        </Trans></p>
         <ul>
-            <li>A <span className="tag">GLOBAL BAN</span> or <TurnTag turn={new ModelTurn(
-                Player.HOST,
-                Action.BAN,
-                Exclusivity.GLOBAL
-            )}/> prevents either player from picking the civilisation in a future turn.
+            <li><Trans i18nKey='instructions.hostAndGuestTurns.7'>A <span className="tag">GLOBAL BAN</span> or
+                <TurnTag turn={new ModelTurn(
+                    Player.HOST,
+                    Action.BAN,
+                    Exclusivity.GLOBAL
+                )}/> prevents either player from picking the civilisation in a future turn.
+            </Trans>
             </li>
-            <li>An <span className="tag">EXCLUSIVE BAN</span> or <TurnTag turn={new ModelTurn(
+            <li><Trans i18nKey='instructions.hostAndGuestTurns.8'>
+                An <span className="tag">EXCLUSIVE BAN</span> or <TurnTag turn={new ModelTurn(
                 Player.HOST,
                 Action.BAN,
                 Exclusivity.EXCLUSIVE
             )}/> prevents only the opponent from picking the civilisation in a future turn and the player from banning
                 the civilisation again in a future turn.
+            </Trans>
             </li>
-            <li>A <span className="tag">NONEXCLUSIVE BAN</span> or <TurnTag turn={new ModelTurn(
+            <li><Trans i18nKey='instructions.hostAndGuestTurns.9'>
+                A <span className="tag">NONEXCLUSIVE BAN</span> or <TurnTag turn={new ModelTurn(
                 Player.HOST,
                 Action.BAN,
                 Exclusivity.NONEXCLUSIVE
             )}/> prevents only the opponent from picking the civilisation in a future turn, and the player may ban the
                 the civilisation again in a future turn, which makes no sense, so you probably want to use <span
-                    className="tag">EXCLUSIVE</span> instead anyway.
+                className="tag">EXCLUSIVE</span> instead anyway.
+            </Trans>
             </li>
         </ul>
-        <p>A <TurnTag turn={new ModelTurn(
+        <p><Trans i18nKey='instructions.hostAndGuestTurns.10'>A <TurnTag turn={new ModelTurn(
             Player.HOST,
             Action.SNIPE,
             Exclusivity.GLOBAL
         )}/> allows a player to remove a civilisation again which the opponent has
-            picked in a previous turn. The GLOBAL, EXCLUSIVE and NONEXCLUSIVE modifiers have no meaning here.</p>
-    </>;
+            picked in a previous turn. The '
+            <span className="tag">GLOBAL</span>,
+            <span className="tag">EXCLUSIVE</span> and
+            <span className="tag">NONEXCLUSIVE</span> modifiers have no meaning here.
+        </Trans></p>
+    </>
+);
 
-const AdminTurns = () =>
+export const HelpAdminTurns = withTranslation()(() =>
     <>
-        <h4>Admin Turns</h4>
-        <p>Admin turns or <TurnTag turn={new ModelTurn(
+        <h4><Trans i18nKey='instructions.adminTurns.title'>Admin Turns</Trans></h4>
+        <p><Trans i18nKey='instructions.adminTurns.1'>Admin turns or <TurnTag turn={new ModelTurn(
             Player.NONE,
             Action.REVEAL_ALL,
             Exclusivity.GLOBAL
         )}/> reveals the previous set of actions which were hidden.
+        </Trans>
         </p>
         <ul>
-            <li>The <span className="tag">REVEAL_PICKS</span>,
-                <span className="tag">REVEAL_BANS</span> and <span className="tag">REVEAL_SNIPES</span> turns reveal the
-                respective hidden actions that were executed before.
-            </li>
-            <li><span className="tag">REVEAL_ALL</span> reveals all hidden actions that were executed before.</li>
+            <li><Trans i18nKey='instructions.adminTurns.2'>
+                The <span className="tag">REVEAL_PICKS</span>,
+                <span className="tag">REVEAL_BANS</span> and
+                <span className="tag">REVEAL_SNIPES</span>
+                turns reveal the respective hidden actions that were executed before.
+            </Trans></li>
+            <li><Trans i18nKey='instructions.adminTurns.3'>
+                <span className="tag">REVEAL_ALL</span> reveals all hidden actions that were executed before.
+            </Trans></li>
         </ul>
-    </>;
+    </>
+);
 
-
-const PresetEditor = () =>
+export const HelpPresetEditor = withTranslation()(() =>
     <>
-        <h4>Preset Editor</h4>
+        <h4><Trans i18nKey='instructions.presetEditor.title'>Preset Editor</Trans></h4>
         <ul>
-            <li>Presets are made up by individual Turns.</li>
-            <li>Each turn has an acting player: The Host, the Guest, or neither of those, (so called "Admin" turns).
-            </li>
-            <li>You can add a Host/Admin/Guest turn by clicking the <button className="button is-small">+
+            <li><Trans i18nKey='instructions.presetEditor.1'>
+                Presets are made up by individual Turns.
+            </Trans></li>
+            <li><Trans i18nKey='instructions.presetEditor.2'>
+                Each turn has an acting player: The Host, the Guest, or neither of those, (so called "Admin" turns).
+            </Trans></li>
+            <li><Trans i18nKey='instructions.presetEditor.3'>
+                You can add a Host/Admin/Guest turn by clicking the <button
+                className="button is-small is-valinged-middle">+
                 New</button> button below the respective column.
-            </li>
-            <li>You can remove a turn by clicking the <button className="delete is-small"/> button to the right of the
-                turn.
-            </li>
+            </Trans></li>
+            <li><Trans i18nKey='instructions.presetEditor.4'>
+                You can remove a turn by clicking the <button className="delete is-small is-valinged-middle"/>
+                button to the right of the turn.
+            </Trans></li>
         </ul>
-    </>;
+    </>
+);
 
-const PresetModifiers = () =>
+export const HelpPresetModifiers = withTranslation()(() =>
     <>
-        <h4>Modifiers</h4>
-        <p>If you want the players to execute two of their turns in parallel, check the <span
+        <h4><Trans i18nKey='instructions.presetModifiers.title'>Modifiers</Trans></h4>
+        <p><Trans i18nKey='instructions.presetModifiers.1'>
+            If you want the players to execute two of their turns in parallel, check the <span
             className="tag">PARALLEL</span> checkbox of the <b>first</b> of those two turns (but do not check
-            it for the second turn).</p>
-        <p>Check the <span className="tag">HIDDEN</span> checkbox to hide the civilisation selected during that turn
-            from the opponent and from spectators until it is revealed by an admin turn (see below)</p>
-    </>;
+            it for the second turn).
+        </Trans></p>
+        <p><Trans i18nKey='instructions.presetModifiers.2'>
+            Check the <span className="tag">HIDDEN</span> checkbox to hide the civilisation selected during that turn
+            from the opponent and from spectators until it is revealed by an admin turn (see below).
+        </Trans></p>
+    </>
+);
 
-const DraftModifiers = () =>
+export const HelpDraftModifiers = withTranslation()(() =>
     <>
-        <h4>Modifiers</h4>
-        <p>A <span className="tag">PARALLEL</span> modifier means both the players to execute their turns in parallel.
+        <h4><Trans i18nKey='instructions.draftModifiers.title'>Modifiers</Trans></h4>
+        <p><Trans i18nKey='instructions.draftModifiers.1'>
+            A <span className="tag">PARALLEL</span> modifier means both the players to execute their turns in parallel.
             The picks or bans are revealed to the other player and spectators only after both the players have made
-            their choices.</p>
-        <p>A <span className="tag">HIDDEN</span> modifier hides the civilisation selected during that turn
-            from the opponent and from spectators until it is revealed by an admin turn (see below)</p>
-    </>;
-
-
-export const HelpBasics = withTranslation()(Basics);
-export const HelpPresetEditor = withTranslation()(PresetEditor);
-export const HelpHostAndGuestTurns = withTranslation()(HostAndGuestTurns);
-export const HelpAdminTurns = withTranslation()(AdminTurns);
-export const HelpPresetModifiers = withTranslation()(PresetModifiers);
-export const HelpDraftModifiers = withTranslation()(DraftModifiers);
+            their choices.
+        </Trans></p>
+        <p><Trans i18nKey='instructions.draftModifiers.2'>
+            A <span className="tag">HIDDEN</span> modifier hides the civilisation selected during that turn
+            from the opponent and from spectators until it is revealed by an admin turn (see below).
+        </Trans></p>
+    </>
+);
