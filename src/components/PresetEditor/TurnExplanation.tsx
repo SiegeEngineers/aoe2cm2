c6001be71d752af578d44eafa64fe579bf6a3e38
HSZemi <hszemi@users.noreply.github.com>
2020-07-11 02:47:38 +0200

    Fix heading and text

--- src/components/PresetEditor/TurnExplanation.tsx
+++ src/components/PresetEditor/TurnExplanation.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import {withTranslation} from "react-i18next";
+import {Trans, withTranslation} from "react-i18next";
 import {HelpAdminTurns, HelpBasics, HelpHostAndGuestTurns, HelpPresetEditor, HelpPresetModifiers} from "./HelpText";
 
 const TurnExplanation = () =>
     <React.Fragment>
-        <h3>Help and Instructions</h3>
+        <h3><Trans i18nKey={'presetEditor.helpAndInstructions'}>Help and Instructions</Trans></h3>
 
         <HelpBasics/>
 
