0ca2172838d281eb4d4694dd91c052ffbcc28a29
Brian Craig <briancraigok@gmail.com>
2020-07-13 11:08:30 -0300

    feat: add spanish translation

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -2,11 +2,13 @@ import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import en_GB from "./languages/en_GB.json";
+import es_ES from "./languages/es_ES.json";
 import de_DE from "./languages/de_DE.json";
 import zh_CN from "./languages/zh_CN.json";
 
 const resources = {
     "en-GB": {translation: en_GB},
+    "es-ES": {translation: es_ES},
     "de-DE": {translation: de_DE},
     "zh-CN": {translation: zh_CN}
 };
