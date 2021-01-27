327637a03508671c5fb6b5d711703020abb03cd7
johannes4998 <johannesstoetzer@gmail.com>
2021-01-27 13:34:36 +0100

    Added missing api connections and grouped them by theme

--- src/components/menu/API.tsx
+++ src/components/menu/API.tsx
@@ -16,14 +16,15 @@ class API extends React.Component<WithTranslation, IState> {
         return (
             <div>
                 <div className="content box" id="API">
-                    <h3><Trans>API stuff</Trans></h3>
+                    <h3><Trans>API</Trans></h3>
+                        <h4>Preset</h4>
                         <h5>Preset</h5>
                             <p>Request the selected preset.</p>
                             <dl>
                                 <dt>Endpoint</dt>
                                 <dd>/api/preset</dd>   
                                 <dt>Request Parameters</dt>
-                                <dd>name</dd>
+                                <dd>id</dd>
                                 <dt>Example Request</dt>
                                 <dd>https://aoe2cm.net/api/preset/simple</dd>
                                 <dd>
@@ -39,6 +40,30 @@ class API extends React.Component<WithTranslation, IState> {
                                     </ul>
                                 </dd>
                             </dl>
+
+                            <h5>Add Preset</h5>
+                            <p>Request the selected preset.</p>
+                            <dl>
+                                <dt>Endpoint</dt>
+                                <dd>/api/preset</dd>   
+                                <dt>Request Parameters</dt>
+                                <dd>No Clue</dd>
+                                <dt>Example Request</dt>
+                                <dd>https://aoe2cm.net/api/preset/new</dd>
+                            </dl>
+
+                            <h5>List Preset</h5>
+                            <p>Lists all presets with name and id.</p>
+                            <dl>
+                                <dt>Endpoint</dt>
+                                <dd>/api/preset/list</dd>   
+                                <dt>Request Parameters</dt>
+                                <dd>No parameters</dd>
+                                <dt>Example Request</dt>
+                                <dd>https://aoe2cm.net/api/preset/list</dd>
+                            </dl>
+
+                            <h4>Draft</h4>
                             <h5>Draft</h5>
                             <p>Request the selected Draft.</p>
                             <dl>
@@ -48,7 +73,52 @@ class API extends React.Component<WithTranslation, IState> {
                                 <dd>name</dd>
                                 <dt>Example Request</dt>
                                 <dd>https://aoe2cm.net/api/draft/FQoju</dd>
-                        </dl>
+                            </dl>
+                            <h5>Add draft</h5>
+                            <p>Request the selected preset.</p>
+                            <dl>
+                                <dt>Endpoint</dt>
+                                <dd>/api/preset</dd>   
+                                <dt>Request Parameters</dt>
+                                <dd>No Clue</dd>
+                                <dt>Example Request</dt>
+                                <dd>https://aoe2cm.net/api/draft/new</dd>
+                            </dl>
+
+                            <h5>Add draft</h5>
+                            <p>Request the selected preset.</p>
+                            <dl>
+                                <dt>Endpoint</dt>
+                                <dd>/api/recentdrafts</dd>   
+                                <dt>Request Parameters</dt>
+                                <dd>No parameters</dd>
+                                <dt>Example Request</dt>
+                                <dd>https://aoe2cm.net/api/recentdrafts</dd>
+                            </dl>
+
+                            
+
+                            <h4>Open Connections</h4>
+                            <p>Shows the number of current connections.</p>
+                            <dl>
+                                <dt>Endpoint</dt>
+                                <dd>/api/connections</dd>   
+                                <dt>Request Parameters</dt>
+                                <dd>No parameters</dd>
+                                <dt>Example Request</dt>
+                                <dd>https://aoe2cm.net/api/connections</dd>
+                            </dl>
+
+                            <h4>Alerts</h4>
+                            <p>Shows the number of current connections.</p>
+                            <dl>
+                                <dt>Endpoint</dt>
+                                <dd>/api/alerts</dd>   
+                                <dt>Request Parameters</dt>
+                                <dd>No Clue</dd>
+                                <dt>Example Request</dt>
+                                <dd>https://aoe2cm.net/api/alerts</dd>
+                            </dl>
                 </div>
             </div>
         );
