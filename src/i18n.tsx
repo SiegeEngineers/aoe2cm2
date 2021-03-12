ca73349a90cb876ae0f6641f6ce29a31e8929a23
Lalit Patel <lalitpatel@users.noreply.github.com>
2021-03-12 22:20:05 +0530

    Minor fixes

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,13 +1,13 @@
 import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
-import availableLocales from "./Locales";
+import resources from "./Locales";
 
 i18n
     .use(initReactI18next)
     .use(LanguageDetector)
     .init({
-        resources: availableLocales,
+        resources: resources,
         fallbackLng: "en-GB",
         interpolation: {
             escapeValue: false // react already protects from xss
