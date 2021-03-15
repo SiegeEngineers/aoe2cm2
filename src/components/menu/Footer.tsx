deca41d0fe5d44ce49a31bc0becf9dc0e26bd999
Lalit Patel <lalitpatel@users.noreply.github.com>
2021-03-15 07:23:30 +0530

    Remove target=_blank from links in footer.

--- src/components/menu/Footer.tsx
+++ src/components/menu/Footer.tsx
@@ -8,20 +8,20 @@ class Footer extends React.Component<object, object> {
                 <div className="container">
                     <div className="content is-small">
                         <p>
-                            Created by <a target="_blank" href="https://aoe2.se">
+                            Created by <a href="https://aoe2.se">
                             <img alt='SiegeEngineers logo' className='inline-logo'
                                  style={{verticalAlign: "text-bottom"}}
                                  src="/images/SiegeEngineers.png"/> Siege Engineers</a> (2019 &ndash; 2020)
                             &bull;
-                            Based on the first version by <span className='is-family-code'>pip</span> &bull;
-                            Contribute on <a target="_blank" href="https://github.com/SiegeEngineers/aoe2cm2">GitHub</a>
+                            Based on the first version by <span className='is-family-code'>pip</span>&nbsp; &bull;
+                            Contribute on <a href="https://github.com/SiegeEngineers/aoe2cm2">GitHub</a>
                             &nbsp;&bull;&nbsp;
                             <Link to="/api">API</Link>
                         </p>
                         <p>
                             Age of Empires II &copy; Microsoft Corporation. <b>Captains Mode for Age of Empires
                             II</b> was created under Microsoft's "<a href="https://www.xbox.com/en-us/developers/rules"
-                                                                     rel="nofollow" target="_blank">
+                                                                     rel='nofollow'>
                             Game Content Usage Rules</a>" using assets from Age of Empires II, and it is not endorsed by
                             or affiliated with Microsoft.
                         </p>
