8b61bcfbc5328fa1bcf746aa2df61370156d90ab
Lalit Patel <lalitpatel@users.noreply.github.com>
2021-03-16 23:34:07 +0530

    Use a drop down instead of a button set as locale switcher

---
+++ src/Locales.tsx
@@ -0,0 +1,35 @@
+import en_GB from "./languages/en_GB.json";
+import es_ES from "./languages/es_ES.json";
+import de_DE from "./languages/de_DE.json";
+import pt_BR from "./languages/pt_BR.json";
+import zh_CN from "./languages/zh_CN.json";
+
+const availableLocales = {
+    "en-GB": {
+        translation: en_GB,
+        displayName: "English",
+        cta: "Switch language to English (UK)"
+    },
+    "es-ES": {
+        translation: es_ES,
+        displayName: "Español",
+        cta: "Cambiar el idioma a español"
+    },
+    "de-DE": {
+        translation: de_DE,
+        displayName: "Deutsch",
+        cta: "Sprache zu Deutsch ändern"
+    },
+    "pt-BR": {
+        translation: pt_BR,
+        displayName: "Português",
+        cta: "Mudar o idioma para português"
+    },
+    "zh-CN": {
+        translation: zh_CN,
+        displayName: "中文",
+        cta: "将语言设置为中文"
+    }
+}
+
+export default availableLocales;
--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,27 +1,13 @@
 import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
-import en_GB from "./languages/en_GB.json";
-import es_ES from "./languages/es_ES.json";
-import de_DE from "./languages/de_DE.json";
-import pt_BR from "./languages/pt_BR.json";
-import zh_CN from "./languages/zh_CN.json";
-
-const resources = {
-    "en-GB": {translation: en_GB},
-    "es-ES": {translation: es_ES},
-    "de-DE": {translation: de_DE},
-    "pt-BR": {translation: pt_BR},
-    "zh-CN": {translation: zh_CN}
-};
-
-console.log(resources);
+import resources from "./Locales";
 
 i18n
     .use(initReactI18next)
     .use(LanguageDetector)
     .init({
-        resources,
+        resources: resources,
         fallbackLng: "en-GB",
         interpolation: {
             escapeValue: false // react already protects from xss
