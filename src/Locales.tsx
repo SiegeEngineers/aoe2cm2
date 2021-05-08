edc98df4c8ab28f3da8981ac0aadd0c54f48ff9f
inductor <kohei.ota@hpe.com>
2021-05-08 14:04:02 +0000

    Add Japanese locale

--- src/Locales.tsx
+++ src/Locales.tsx
@@ -1,6 +1,7 @@
 import en_GB from "./languages/en_GB.json";
 import es_ES from "./languages/es_ES.json";
 import de_DE from "./languages/de_DE.json";
+import ja_JP from "./languages/ja_JP.json";
 import pt_BR from "./languages/pt_BR.json";
 import zh_CN from "./languages/zh_CN.json";
 
@@ -19,6 +20,11 @@ const availableLocales = {
         translation: de_DE,
         displayName: "Deutsch",
         cta: "Sprache zu Deutsch ändern"
+    },
+    "ja-JP": {
+        translation: ja_JP,
+        displayName: "日本語",
+        cta: "言語を日本語に切替"
     },
     "pt-BR": {
         translation: pt_BR,
