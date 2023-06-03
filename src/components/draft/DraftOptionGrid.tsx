8072bbac58e68fd82fab1426d57c5e10ff8062c6
HSZemi <hszemi@users.noreply.github.com>
2023-06-03 16:50:26 +0200

    Sort draft options in grid according to selected locale

--- src/components/draft/DraftOptionGrid.tsx
+++ src/components/draft/DraftOptionGrid.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import DraftOptionPanel from "../../containers/DraftOptionPanel";
 import DraftOptionPanelType from "../../constants/DraftOptionPanelType";
 import DraftOption from "../../models/DraftOption";
+import i18next from "i18next";
+import i18n from "i18next";
 
 interface IProps {
     draftOptions: DraftOption[]
@@ -10,7 +12,7 @@ interface IProps {
 class DraftOptionGrid extends React.Component<IProps, object> {
     public render() {
 
-        const panels = this.props.draftOptions.map((draftOption, index) => {
+        const panels = this.props.draftOptions.sort(this.compareDraftOptions).map((draftOption, index) => {
             return React.createElement(DraftOptionPanel, {
                 draftOptionPanelType: DraftOptionPanelType.CHOICE,
                 active: false,
@@ -36,6 +38,12 @@ class DraftOptionGrid extends React.Component<IProps, object> {
                 </div>
             </div>
         );
+    }
+
+    private compareDraftOptions(a: DraftOption, b: DraftOption) {
+        const aName = i18next.t(['civs.' + a.name, a.name]);
+        const bName = i18next.t(['civs.' + b.name, b.name]);
+        return aName.localeCompare(bName, i18n.language);
     }
 }
 
